07236fcf7ed431e4049b5ccadef98f6c71be9652
helldefender <1359728817@qq.com>
2017-12-02 22:18:49 +0800

    Finish GrepFilter

--- fairy-client/src/main/java/me/zane/fairy/repository/GrepFilter.java
+++ fairy-client/src/main/java/me/zane/fairy/repository/GrepFilter.java
@@ -4,6 +4,9 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.Transformations;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import me.zane.fairy.vo.LogcatContent;
 
 /**
@@ -19,7 +22,46 @@ public class GrepFilter {
         return Transformations.map(rawData, logcatData -> {
 
             // TODO: 2017/11/30 grep logic
-            return new LogcatContent(0, "");
+            LogcatContent logcatContent = rawData.getValue();
+            String rawContent = "";
+            if (logcatContent != null && ((rawContent = logcatContent.getContent()).length() > 0)) {
+                return new LogcatContent(0, parseHtml(rawContent, grep));
+            }
+            return new LogcatContent(0, rawContent);
         });
     }
+
+    private static String parseHtml(String rawContent, String grep) {
+        Pattern pattern = Pattern.compile("(<(p)><font(\\s[^\\t\\n\\r\\>]+)?>([^<>])*" + grep + "([^<>])*<\\/p>)+?");
+        Matcher matcher = pattern.matcher(rawContent);
+        StringBuilder stringBuilder = new StringBuilder();
+        while (matcher.find()) {
+            stringBuilder.append(matcher.group());
+        }
+        return stringBuilder.toString();
+    }
+
+    //    private static String parseHtml(String rawContent, String grep) {
+//        Pattern pattern = Pattern.compile("<p>(.*?)</p>");
+//        Matcher matcher = pattern.matcher(rawContent);
+//        Pattern filterPattern = Pattern.compile(".*"+grep+".*");
+//        StringBuilder stringBuilder = new StringBuilder();
+//        while (matcher.find()) {
+//            if (filterPattern.matcher(matcher.group()).matches()) {
+//                stringBuilder.append(matcher.group());
+//            }
+//        }
+//        return stringBuilder.toString();
+//    }
+
+    public String parseHtml1(String rawContent,String grep) {
+        Pattern pattern = Pattern.compile("(<(p)><font(\\s[^\\t\\n\\r\\>]+)?>([^<>])*" + grep + "([^<>])*<\\/p>)+?");
+        Matcher matcher = pattern.matcher(rawContent);
+        StringBuilder stringBuilder = new StringBuilder();
+        while (matcher.find()) {
+            stringBuilder.append(matcher.group());
+        }
+        return stringBuilder.toString();
+    }
+
 }
